a82396b7e9a4c70ed62ca3154ef1519e840ba472
tony-molumby <tony-molumby@gmail.com>
2019-01-10 22:00:53 -0800

    added elements to home page

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,10 +1,14 @@
 import React from 'react'
 import Social from './Social'
 
+import headshot from '../images/Headshot.png'
+
 export default (props) => (
-  <div>
-    <h1>Home</h1>
+  <div className='home'>
+    <h1 className='red-text-box name-heading shadow'>TONY MOLUMBY</h1>
+    <div className='headshot' ></div>
     <Social />
+    <h2 className='subheading red-text-box shadow'>Web Developer, Product Owner and Clinician</h2>
   </div>
  
 )
