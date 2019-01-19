0c56cb123dbae0cbe81fab9b8b1abf45351ed6ce
tony-molumby <25395668+tony-molumby@users.noreply.github.com>
2019-01-18 17:08:59 -0800

    started form integration

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,39 +1,14 @@
 import React from 'react'
 
+import ContactForm from './ContactForm'
+
 export default (props) => (
   <div className='page'>
-    <div id='mywork-banner' className='banner'></div>
-    <h2 id='mywork-subheading' className='shadow red-text-box subheading page-subheading'>With His Newfound Powers...</h2>
-    <div id='mywork-content' className='content'>
-      <p>
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
-        Aenean euismod bibendum laoreet. Proin gravida dolor sit 
-        amet lacus accumsan et viverra justo commodo. Proin sodales 
-        pulvinar sic tempor. Sociis natoque penatibus et magnis dis 
-        parturient montes, nascetur ridiculus mus. Nam fermentum, 
-        nulla luctus pharetra vulputate, felis tellus mollis orci, 
-        sed rhoncus pronin sapien nunc accuan eget.
-      </p>
-      <p>
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
-        Aenean euismod bibendum laoreet. Proin gravida dolor sit 
-        amet lacus accumsan et viverra justo commodo. Proin sodales 
-        pulvinar sic tempor. Sociis natoque penatibus et magnis dis 
-        parturient montes, nascetur ridiculus mus. Nam fermentum, 
-        nulla luctus pharetra vulputate, felis tellus mollis orci, 
-        sed rhoncus pronin sapien nunc accuan eget.
-      </p>
-      <p>
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
-        Aenean euismod bibendum laoreet. Proin gravida dolor sit 
-        amet lacus accumsan et viverra justo commodo. Proin sodales 
-        pulvinar sic tempor. Sociis natoque penatibus et magnis dis 
-        parturient montes, nascetur ridiculus mus. Nam fermentum, 
-        nulla luctus pharetra vulputate, felis tellus mollis orci, 
-        sed rhoncus pronin sapien nunc accuan eget.
-      </p>
+    <div id='contact-banner' className='banner'></div>
+    <h2 id='contact-subheading' className='shadow red-text-box subheading page-subheading'>A Call Goes Out To Our Hero</h2>
+    <div id='contact-content' className='content'>
+      <ContactForm />
     </div>
-    {/* <h2 id='about-action-text'>Call to Action Text</h2> */}
     
   </div>
 )
---
+++ src/components/ContactForm.js
@@ -0,0 +1,88 @@
+import React, {Component} from 'react';
+
+class ContactForm extends Component {
+    constructor(props){
+        super(props);
+        this.state= {
+            name: "",
+            email: "",
+            message: "",
+            nameErr: "",
+            emailErr: "",
+            messageErr: "",
+            sendMessage: "",
+        }
+    }
+
+    handleChange = (e) => {
+        this.setState({
+            [e.target.name]: e.target.value
+        });
+    }
+
+    render(){
+        let {
+                name, 
+                email, 
+                message, 
+                nameErr,
+                emailErr,
+                messageErr,
+                sendMessage
+            } = this.state;
+        
+        return (
+            <div id={this.props.id}>
+                <form
+                    action="https://formspree.io/tony.molumby@gmail.com"
+                    method="POST"
+                >
+                    <div id="name">
+                        {nameErr}
+                        <input
+                            placeholder="name"
+                            type="text"
+                            name='name' 
+                            className="inputs" 
+                            value={name}
+                            onChange={this.handleChange}
+                        />
+                    </div>
+                    <div id="email">
+                        {emailErr}
+                        <input 
+                            placeholder="email" 
+                            className="inputs" 
+                            name="email"
+                            type='email'
+                            value={email}
+                            onChange={this.handleChange}
+                        />
+                    </div>
+                    <div id="message">
+                        {messageErr}
+                        <textarea
+                            placeholder="type in a message" 
+                            className="message"
+                            name="message"
+                            value={message} 
+                            onChange={this.handleChange}
+                        ></textarea>
+                    </div>
+                    <div id="subject">
+                        <input
+                            type="hidden"
+                            className="message"
+                            name="_subject"
+                            value="New Message from your portfolio site!"
+                        />
+                    </div>
+                    <input id="submit" type="submit" value='send' />
+                    <div id="send-message">{sendMessage}</div>
+                </form>
+            </div>
+        )
+    }
+}
+
+export default ContactForm;
