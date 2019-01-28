3ebcf6bb1db295789a7f61ae89ab6bd0fbd04d65
tony-molumby <25395668+tony-molumby@users.noreply.github.com>
2019-01-27 20:56:59 -0800

    finished card animation

--- src/components/Card.js
+++ src/components/Card.js
@@ -15,26 +15,42 @@ export default ({title, icon, subtitle, src, content}) => {
    //animation references   
     let zoom = useRef(null)
     let zoomHeader = useRef(null)
+    let stretchCard = useRef(null)
 
     const [cardSize, setCardSize] = useGlobal('cardSize')
     const [isZoomed, setIsZoomed] = useGlobal('isZoomed')
 
     function zoomIn() {
         setIsZoomed(true)
+        let moveX = '0px'
+        let zoomFactor = 1.5
+        let cardWidth = (parseInt(cardSize.width))
+        let screenCenter = (window.innerWidth
+            || document.documentElement.clientWidth
+            || document.body.clientWidth) / 2
+        let cardTop = -cardRef.getBoundingClientRect().top + 50 + 'px'
+        let cardLeft = cardRef.getBoundingClientRect().left 
+        let cardCenter = cardLeft + (cardWidth * zoomFactor / 2)
         document.body.style.overflow = 'hidden'
-        let cardTop = -cardRef.getBoundingClientRect().top + 'px'
-        let cardLeft = -cardRef.getBoundingClientRect().left + 'px'
+        moveX = screenCenter - cardCenter 
+        
+        console.log('screenCenter: ' + screenCenter)
+        console.log('move: ' + moveX)
         console.log('top: ' + cardTop)
-        console.log('left: ' + cardLeft)
+        console.log('left: ' + cardLeft) 
+        console.log('center: ' + cardCenter)
+        
         zoom.current = new TimelineLite()
-        zoom.current.to(cardRef, 0.2, {
-            position: 'fixed',
-            top: 0
-            // left: cardLeft,
-        }).to(cardRef, 0.4, {
-            width: '70%',
-            height: 'auto'
+        zoom.current.to(cardRef, 0.1, {
+            position: 'relative',
+            top: cardTop,
+            left: moveX
+        }).to(cardRef, 0.2, {
+            width: zoomFactor * 100 + '%' ,
+            height: zoomFactor * 100 + '%'
         })
+
+        
 
         zoom.current.play()
             
@@ -49,10 +65,10 @@ export default ({title, icon, subtitle, src, content}) => {
 
     function zoomOut() {
         setIsZoomed(false)
+        cardRef.style.width = cardSize.width
+        cardRef.style.height = cardSize.height
         document.body.style.overflowY = 'scroll'
         zoom.current.reversed(true)
-        // zoomHeader.current.reversed(true)
-
     }
 
     function toggleZoom() {
@@ -60,36 +76,37 @@ export default ({title, icon, subtitle, src, content}) => {
     }
 
     return (
-        <div 
-            className='card'
-            ref={div => cardRef = div}
-            onClick={!isZoomed ? toggleZoom : null}
-            style={cardSize}
-            >
-            <CardHeader 
-                title={title}
-                icon={icon}
-                subtitle={subtitle}
-                src={src}
-                cardHeaderRef={cardHeaderRef}
-                isZoomed={isZoomed}
-                toggleZoom={toggleZoom}
-                />
-            <Transition in={isZoomed} timeout={200} >
-                {(state) => (
-                    <div 
-                        className='content'
-                        style={{
-                            ...styles.content.default,
-                            ...styles.content.transition[state]
-                        }}
-                        >
-                        {content}
-                    </div>
-                )}
-            </Transition>
+        <div className='card-container'>
+            <div 
+                className='card'
+                ref={div => cardRef = div}
+                onClick={!isZoomed ? toggleZoom : null}
+                style={cardSize}
+                >
+                <CardHeader 
+                    title={title}
+                    icon={icon}
+                    subtitle={subtitle}
+                    src={src}
+                    cardHeaderRef={cardHeaderRef}
+                    isZoomed={isZoomed}
+                    toggleZoom={toggleZoom}
+                    />
+                <Transition in={isZoomed} timeout={200} >
+                    {(state) => (
+                        <div 
+                            className='content'
+                            style={{
+                                ...styles.content.default,
+                                ...styles.content.transition[state]
+                            }}
+                            >
+                            {content}
+                        </div>
+                    )}
+                </Transition>
+            </div>
         </div>
-        
 
         
     )
